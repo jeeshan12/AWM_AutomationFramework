cf882456c6c1a1cde270f9d6cbd9518f9a0c0952
jeeshan12 <mohdjeeshan007@gmail.com>
2024-02-21 18:18:59 +0530

    fix(singleton): Fixed the issues related to sonar lint for redis connector

--- src/main/java/org/kira/automation/connector/RedisConnector.java
+++ src/main/java/org/kira/automation/connector/RedisConnector.java
@@ -7,14 +7,11 @@ import redis.clients.jedis.JedisPoolConfig;
 public class RedisConnector {
 
   private RedisConnector() {
-    if (RedisConnectorHelper.REDIS_CONNECTOR != null) {
       throw new IllegalStateException("Singleton instance already exists for Redis connector");
-    }
   }
 
   private static class RedisConnectorHelper {
     private static final RedisConnector REDIS_CONNECTOR = new RedisConnector();
-    // If your Redis server requires authentication, you can use REDIS_CONNECTOR.auth("password");
   }
 
   public static RedisConnector getInstance() {
@@ -23,8 +20,9 @@ public class RedisConnector {
 
   public Jedis getRedisConnection(final String url, final int port) {
     JedisPoolConfig poolConfig = new JedisPoolConfig();
-    JedisPool jedisPool = new JedisPool(poolConfig, url, port);
-    return jedisPool.getResource();
+    try (JedisPool jedisPool = new JedisPool(poolConfig, url, port)) {
+      return jedisPool.getResource();
+    }
   }
 
 }
