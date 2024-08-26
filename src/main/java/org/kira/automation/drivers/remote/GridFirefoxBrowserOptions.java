145ecd3d0095dbc587deba313a0dffa4e5da8229
jeeshan12 <mohdjeeshan007@gmail.com>
2024-08-26 11:10:36 +0200

    fix(grid): fixed the headless execution for grid

--- src/main/java/org/kira/automation/drivers/remote/GridFirefoxBrowserOptions.java
+++ src/main/java/org/kira/automation/drivers/remote/GridFirefoxBrowserOptions.java
@@ -1,6 +1,10 @@
 package org.kira.automation.drivers.remote;
 
+import static org.kira.automation.constants.FrameworkConstants.HEADLESS;
+import static org.kira.automation.drivers.web.FirefoxOptionsDecorator.FIREFOX_HEADLESS_DECORATOR;
+
 import java.util.Map;
+import java.util.Optional;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.CapabilityType;
@@ -9,8 +13,9 @@ public class GridFirefoxBrowserOptions implements IGridBrowserOptions {
 
   @Override
   public MutableCapabilities getGridBrowserOptions(Map<String, String> browserOptionsMap) {
-    MutableCapabilities firefoxOptions = new FirefoxOptions();
-
+    FirefoxOptions firefoxOptions = new FirefoxOptions();
+    var isHeadless = Optional.of(Boolean.parseBoolean(System.getProperty(HEADLESS))).orElse(true);
+    FIREFOX_HEADLESS_DECORATOR.accept(isHeadless, firefoxOptions);
     firefoxOptions.setCapability(
       CapabilityType.BROWSER_VERSION,
       browserOptionsMap.get(CapabilityType.BROWSER_VERSION)
